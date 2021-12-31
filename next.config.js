81ff12c5bb28f9aba8fd1f53cf600dd6b43d9e0e
William Li <email@williamli.dev>
2021-12-31 18:52:48 +0800

    remove rewrite rule

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@ module.exports = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      {
-        source: '/api/download/:id',
-        destination: 'https://nask.hk/',
-      },
+      
     ]
   },
 }
