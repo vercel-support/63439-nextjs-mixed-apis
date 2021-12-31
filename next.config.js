1d839b57580929b64dbd9481994fb1f2432b2b8e
William Li <email@williamli.dev>
2021-12-31 18:48:12 +0800

    more hacks

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         source: '/api/download/:id',
-        destination: '/pages/api/download/[id]',
+        destination: '/pages/api/download/[id].js',
       },
     ]
   },
