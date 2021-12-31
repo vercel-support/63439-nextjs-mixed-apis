a9deb3226a684ce0d3c3c3c6e410c1f7ad4e4cb3
William Li <email@williamli.dev>
2021-12-31 19:00:59 +0800

    try afterFiles

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   async rewrites() {
     return {
-      fallback: [
+      afterFiles: [
         {
           source: '/api/download/:id',
           destination: '/api/attachment/[id].js',
